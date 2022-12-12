ed379e57713a747372572f0fd0f7bef5be8f4fc8
Muhammad Fida Raditya <91618887+pusingbro@users.noreply.github.com>
2022-12-12 20:37:33 +0700

    Maaf belum bang, alasannya ada di extend commitnya ya bang
    hari ini (12/12/2022 20:36) saya mau mempersiapkan UAS jarkom bang, jadi belum bisa kerjakan praktikum ke-6 nya, kemarin baru sampai ke-5 dan kebingungan juga bang. Mohon maaf sekali bang

---
+++ PraktikumMG6/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>AJAX</title>
+  </head>
+  <body>
+    <header>
+      <h1>TUGAS AJAX</h1>
+      <p style="margin-bottom: 2pt">Disusun Oleh Muhammad Fida Raditya</p>
+      <p style="margin-top: 2pt">NIM 120140037</p>
+    </header>
+    
+    <div>
+      <form id="getdata" method="post"></form>
+    </div>
+
+
+    
+
+
+
+    <script
+      src="https://code.jquery.com/jquery-3.6.1.min.js"
+      integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
+      crossorigin="anonymous"
+    ></script>
+    <script>
+      $(document).ready(function () {
+                
+      })
+    </script>
+  </body>
+</html>
